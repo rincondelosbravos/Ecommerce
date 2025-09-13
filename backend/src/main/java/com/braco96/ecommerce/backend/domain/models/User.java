c3b07e75733fed20ff6ab206a767bdbdda4dbc16
braco96 <bravocolladoluis@gmail.com>
2025-09-13 18:34:06 +0200

    UserTypes y configuracion de LOOMBOK Y MAPSTRUCTURE

---
+++ backend/src/main/java/com/braco96/ecommerce/backend/domain/models/User.java
@@ -0,0 +1,8 @@
+package com.braco96.ecommerce.backend.domain.models;
+
+import lombok.Data;
+
+@Data
+
+public class User {
+}
---
+++ backend/src/main/java/com/braco96/ecommerce/backend/domain/models/UserType.java
@@ -0,0 +1,5 @@
+package com.braco96.ecommerce.backend.domain.models;
+
+public enum UserType {
+    ADMIN, USER
+}
